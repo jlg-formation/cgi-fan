e9e98c1fa45e4e22fb56a4bbccae4ad674ee3d00
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-04-11 17:03:18 +0200

    add webpack

--- app/app.js
+++ app/app.js
@@ -1,32 +1,37 @@
-(function() {
-	'use strict';
+'use strict';
 
-	var app = angular.module('main', ['cgi-list', 'cgi-route']);
+require('./style.css');
 
-	app.config(function() {
-		console.log('config main');
-	});
+require('angular');
+require('angular-ui-router');
+require('./cgi-list/cgi-list.js');
+require('./cgi-route/cgi-route.js');
 
-	app.run(function() {
-		console.log('run main');
-	});
+var app = angular.module('main', ['cgi-list', 'cgi-route']);
 
-	app.directive('cgiHeader', function() {
-		return {
-			restrict: 'E',
-			templateUrl: './tmpl/cgi-header.html',
-		};
-	});
-	app.directive('cgiBody', function() {
-		return {
-			restrict: 'E',
-			templateUrl: './tmpl/cgi-body.html',
-		};
-	});
-	app.directive('cgiFooter', function() {
-		return {
-			restrict: 'E',
-			templateUrl: './tmpl/cgi-footer.html',
-		};
-	});
-})();
+app.config(function() {
+	console.log('config main');
+});
+
+app.run(function() {
+	console.log('run main');
+});
+
+app.directive('cgiHeader', function() {
+	return {
+		restrict: 'E',
+		templateUrl: './tmpl/cgi-header.html',
+	};
+});
+app.directive('cgiBody', function() {
+	return {
+		restrict: 'E',
+		templateUrl: './tmpl/cgi-body.html',
+	};
+});
+app.directive('cgiFooter', function() {
+	return {
+		restrict: 'E',
+		templateUrl: './tmpl/cgi-footer.html',
+	};
+});

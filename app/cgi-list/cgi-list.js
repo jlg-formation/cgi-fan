0d81c978936ea05b838b7ab79d581c16fef3f31b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-04-11 11:58:50 +0200

    Add : function

--- app/cgi-list/cgi-list.js
+++ app/cgi-list/cgi-list.js
@@ -3,7 +3,8 @@
 
 	var app = angular.module('cgi-list', []);
 
-	app.run(['$rootScope', function($rootScope) {
+	app.run(function($rootScope) {
+		'ngInject';
 		$rootScope.affaires = [];
 
 		for (var i = 0; i < 10000; i++) {
@@ -12,7 +13,8 @@
 				description: 'description' + i,
 			});
 		}
-	}]);
+		console.log('$rootScope.affaires', $rootScope.affaires);
+	});
 
 	app.directive('cgiList', function() {
 		return {

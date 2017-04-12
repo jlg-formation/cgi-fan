9e366fc4258e65a2c755a5fb80e96b84c66a01d2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-04-12 12:35:32 +0200

    Modify: promises, add webservices.json

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const serveIndex = require('serve-index');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const webpackDevMiddleware = require('webpack-dev-middleware');
+const ws = require('./webservices.js');
 
 var app = express();
 
 webpackConfig.output.path = '/';
 const compiler = webpack(webpackConfig);
 app.use('/app/wpk/', webpackDevMiddleware(compiler, {}));
+
+app.use('/ws', ws);
 
 app.use(express.static('.'));
 app.use(serveIndex('.', {
@@ -20,7 +23,9 @@ app.use(serveIndex('.', {
 
 app.use(['/app/produits', '/app/services', '/app/contact'], function(req, res, next) {
 	console.log('URL rewriting', req.url);
-	res.sendFile('./app/index.html', {root:__dirname});
+	res.sendFile('./app/index.html', {
+		root: __dirname
+	});
 });
 
 app.use(function(req, res, next) {

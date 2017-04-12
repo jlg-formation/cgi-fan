51039e7ac7b166b6644d3a8fd5434749efcb6cfc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-04-12 12:26:40 +0200

    Add: promises

--- app/cgi-route/cgi-route.js
+++ app/cgi-route/cgi-route.js
@@ -18,7 +18,9 @@
 		var produitState = {
 			name: 'produits',
 			url: '/produits',
-			templateUrl: './cgi-route/tmpl/produits.html'
+			templateUrl: './cgi-route/tmpl/produits.html',
+			controller: 'ProduitsCtrl',
+			controllerAs: '$ctrl'
 		}
 
 		var serviceState = {
@@ -40,4 +42,29 @@
 		$urlRouterProvider.otherwise('/');
 	});
 
+	app.controller('ProduitsCtrl', function ProduitsCtrl($http, $q) {
+		'ngInject';
+		console.log('ProduitsCtrl', arguments);
+		var ctrl = this;
+		ctrl.start = function() {
+			console.log('start', arguments);
+			$http.get('../ws/s1').then(function(response) {
+				console.log('response', response);
+				return $q.all([
+					$http.get('../ws/s2').then(function(response) {
+						return $http.get('../ws/s6');
+					}),
+					$http.get('../ws/s3'),
+					$http.get('../ws/s4')
+				]);
+			}).then(function(responses) {
+				console.log('responses', responses);
+				return $http.get('../ws/s5');
+			}).then(function(response) {
+				console.log('response', response);
+			}).catch(function(error) {
+				console.error('error', error);
+			});
+		};
+	});
 })();
